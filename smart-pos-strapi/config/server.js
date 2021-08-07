db7798fca1bd2b66a33484e2ecbf1fd14450d9b5
Serge Harb <me@sergeharb.com>
2021-08-07 12:43:44 +0300

    Initial commit

---
+++ smart-pos-strapi/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '647f91e69fd3dc4201c659dbbaff986d'),
+    },
+  },
+});
